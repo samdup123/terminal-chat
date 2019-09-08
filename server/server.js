94b7c448728c4f2bd3fbd9811aac1be8fe692b64
sam <samdup123@gmail.com>
2019-09-07 22:59:18 -0400

    that just worked, shave me down and call me a mole rat

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const expressWs = require('express-ws')(app);
+
+app.get('/', function(req, res, next){
+  console.log('get route', req.testing);
+  res.end();
+});
+
+app.ws('/', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+  console.log('socket', req.testing);
+});
+ 
+app.listen(3000);
